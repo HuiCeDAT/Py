2d744ade7eaac58105101a7f6aa8d3db2d039f7b
HuiCeDAT <3049525228@qq.com>
2025-04-16 11:30:46 +0800

    commit

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
-print(123)
+def max_subarray_sum(nums):
+    if not nums:
+        return 0
+    current_sum = max_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+
+
+# 测试示例
+nums1 = [1, -2, 3, 5, -1]
+nums2 = [1, -2, 3, -8, 5, 1]
+nums3 = [1, -2, 3, -2, 5, 1]
+print(max_subarray_sum(nums1))
+print(max_subarray_sum(nums2))
+print(max_subarray_sum(nums3))
